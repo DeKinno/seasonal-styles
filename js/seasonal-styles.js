303794fc806ae6cad2605f7fe9f609f845ea433d
DeKinno <egnavas173@gmial.com>
2022-07-28 15:19:47 -0700

    finised

--- js/seasonal-styles.js
+++ js/seasonal-styles.js
@@ -3,5 +3,41 @@ $("document").ready(function(){
         e.preventDefault();//stop default submission
         var season = $(this).attr("href");//contents of href attribute of this element
         alert(season);
+
+/////////////SPRING/////////////////////////////
+        if(season == 'Spring'){
+        $("html").css("background-color", "#2B7129");
+        $("#wear").attr("src","images/spring-wear.jpg");
+        $("#logo").attr("src","images/spring");
+        $("header h3").text("jump into spring")
+    }
+/////////////SUMMER//////////////////////////////
+        if(season == 'Summer'){
+        $("html").css("background-color", "#EBA52B");
+        $("#wear").attr("src","images/summer-wear.jpg");
+        $("#logo").attr("src","images/summer");
+        $("header h3").text("jump into summer")
+    }
+//////////////FALL//////////////////////////////////
+        if(season == 'Fall'){
+        $("html").css("background-color", "#A81124");
+        $("#wear").attr("src","images/fall-wear.jpg");
+        $("#logo").attr("src","images/fall");
+        $("header h3").text("jump into fall")
+    }
+///////////////////////WINTER////////////////////////////
+    if(season == 'Winter'){
+    $("html").css("background-color", "#005393");
+    $("#wear").attr("src","images/winter-wear.jpg");
+    $("#logo").attr("src","images/winter");
+    $("header h3").text("jump into winter")
+}
+/////////////////////////DEFUALT/////////////////////
+    if(season == 'Defualt'){
+    $("html").css("background-color", "#000000");
+    $("#wear").attr("scr","images/defualt.jpg")
+    $("#logo").attr("scr","images/four-seasons.gif")
+    $("header h3").text("jump into Defualt")
+    }
     });
 });
